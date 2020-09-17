0f109f2643c667b167e1ead6eb5bd551a8f478e6
purirahul <rpuri648@gmail.com>
2020-09-17 21:14:46 +0530

    First example node express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+
+const hostname = "localhost";
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+
+    console.log(req.headers);
+    res.setStatuscode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.end("<html><body><h1>This is a express server</h1></body></html>");
+
+});
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
