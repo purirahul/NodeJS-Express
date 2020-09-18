a7bb12ce9331646648fe4e8d7bc1af7f48917f83
purirahul <rpuri648@gmail.com>
2020-09-18 22:54:19 +0530

    Added endpoints for leaders and leaders/:leaderId

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = "localhost";
 const port = 3000;
@@ -12,7 +13,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter)
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 
 app.use(express.static(__dirname+ '/public'));
